a4239159d612005f19d217aa1a340121113844ec
Luongphamgiavy@gmail.com <49144082+giavy0209@users.noreply.github.com>
2020-04-24 22:35:41 +0700

    a

---
+++ build/precache-manifest.eca211f9621612adcba7f8fc5b6bbe8c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eb3149242e6dfdb104326a4a8c98820",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03cf68a8cb2dd149860e",
+    "url": "/static/css/main.82be8d23.chunk.css"
+  },
+  {
+    "revision": "c27fd850dca614715038",
+    "url": "/static/js/2.bab412fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bab412fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03cf68a8cb2dd149860e",
+    "url": "/static/js/main.cd8a843e.chunk.js"
+  },
+  {
+    "revision": "d4f9768a8cdc9e3fac5c",
+    "url": "/static/js/runtime-main.6fda849d.js"
+  },
+  {
+    "revision": "74df8d00be469e21fe01eb7869c472a4",
+    "url": "/static/media/bg-day.74df8d00.jpg"
+  },
+  {
+    "revision": "348085f23d8d4b47e0d6f0d3f9ed55f8",
+    "url": "/static/media/bg.348085f2.jpg"
+  },
+  {
+    "revision": "4ec8ffdecd08f8c5fa0154588aa04b7a",
+    "url": "/static/media/bird-1.4ec8ffde.gif"
+  },
+  {
+    "revision": "b64e75322edb4fa855b687be15b525c7",
+    "url": "/static/media/bird-2.b64e7532.gif"
+  },
+  {
+    "revision": "07ca7c8c512365e49f7d9d0fa3766e2e",
+    "url": "/static/media/bird-3.07ca7c8c.gif"
+  },
+  {
+    "revision": "7462c66b7ef8d7bcf31bd332cd0bbc6c",
+    "url": "/static/media/bird-4.7462c66b.gif"
+  },
+  {
+    "revision": "3dfe6380d68c26575e44ee679395845f",
+    "url": "/static/media/bird-5.3dfe6380.gif"
+  },
+  {
+    "revision": "615f93437175bfa529fae9102f9d25a6",
+    "url": "/static/media/cloud-1.615f9343.gif"
+  },
+  {
+    "revision": "4e2cc3de257899d8d0b57fad8399af8a",
+    "url": "/static/media/cloud.4e2cc3de.gif"
+  },
+  {
+    "revision": "ceecbeb53f0b0c93d6b25745be759ab2",
+    "url": "/static/media/sun.ceecbeb5.gif"
+  }
+]);
